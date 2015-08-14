ebf96cabfee9e5e832b27b60afe1a8e0ebda24c1
Nishant Patil <nishantapatil@gmail.com>
2015-08-14 17:49:51 +0530

    Payment hack for success.

--- PayU/src/com/payu/sdk/ProcessPaymentActivity.java
+++ PayU/src/com/payu/sdk/ProcessPaymentActivity.java
@@ -4,19 +4,15 @@ import android.app.AlertDialog;
 import android.content.BroadcastReceiver;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.support.v4.app.FragmentActivity;
-import android.view.View;
-import android.webkit.JavascriptInterface;
+import android.util.Log;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
-import com.payu.custombrowser.Bank;
-import com.payu.custombrowser.PayUWebChromeClient;
 import org.apache.http.util.EncodingUtils;
 
 public class ProcessPaymentActivity extends FragmentActivity {
@@ -25,11 +21,12 @@ public class ProcessPaymentActivity extends FragmentActivity {
     boolean cancelTransaction = false;
 
     @Override
+    @android.webkit.JavascriptInterface
     protected void onCreate(Bundle savedInstanceState) {
-        if(savedInstanceState!=null){
+        if (savedInstanceState != null) {
             super.onCreate(null);
             finish();//call activity u want to as activity is being destroyed it is restarted
-        }else {
+        } else {
             super.onCreate(savedInstanceState);
         }
 
@@ -38,80 +35,57 @@ public class ProcessPaymentActivity extends FragmentActivity {
         webView = (WebView) findViewById(R.id.webview);
         webView.getSettings().setCacheMode(WebSettings.LOAD_NO_CACHE);
 
-       try {
-           Class.forName("com.payu.custombrowser.Bank");
-           final Bank bank = new Bank() {
-                @Override
-                public void registerBroadcast(BroadcastReceiver broadcastReceiver, IntentFilter filter) {
-                    mReceiver = broadcastReceiver;
-                    registerReceiver(broadcastReceiver, filter);
-                }
 
-                @Override
-                public void unregisterBroadcast(BroadcastReceiver broadcastReceiver) {
-                    if(mReceiver != null){
-                        unregisterReceiver(mReceiver);
-                        mReceiver = null;
+        webView.getSettings().setSupportMultipleWindows(true);
+        webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
+        webView.addJavascriptInterface(new Object() {
+            @android.webkit.JavascriptInterface
+            public void onSuccess() {
+                onSuccess("");
+            }
+
+            @android.webkit.JavascriptInterface
+            public void onSuccess(final String result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent();
+                        intent.putExtra("result", result);
+                        setResult(RESULT_OK, intent);
+                        finish();
                     }
-                }
-
-                @Override
-                public void onHelpUnavailable() {
-                    findViewById(R.id.parent).setVisibility(View.GONE);
-                    findViewById(R.id.trans_overlay).setVisibility(View.GONE);
-                }
-
-                @Override
-                public void onBankError() {
-                    findViewById(R.id.parent).setVisibility(View.GONE);
-                    findViewById(R.id.trans_overlay).setVisibility(View.GONE);
-                }
-
-                @Override
-                public void onHelpAvailable() {
-                    findViewById(R.id.parent).setVisibility(View.VISIBLE);
-                }
-            };
-            Bundle args = new Bundle();
-            args.putInt("webView", R.id.webview);
-            args.putInt("tranLayout",R.id.trans_overlay);
-            args.putInt("mainLayout",R.id.r_layout);
-
-            String [] list =  getIntent().getExtras().getString("postData").split("&");
-            String txnId = null;
-            for (String item : list) {
-                if(item.contains("txnid")){
-                    txnId = item.split("=")[1];
-                    break;
-                }
+//                }
+                });
             }
-            txnId = txnId == null ? String.valueOf(System.currentTimeMillis()) : txnId;
-            args.putString(Bank.TXN_ID, txnId);
-            if(getIntent().getExtras().containsKey("showCustom")) {
-                args.putBoolean("showCustom", getIntent().getBooleanExtra("showCustom", false));
-            }
-            args.putBoolean("showCustom", true);
-            bank.setArguments(args);
-            findViewById(R.id.parent).bringToFront();
-            try {
-                getSupportFragmentManager().beginTransaction().setCustomAnimations(R.anim.fade_in, R.anim.cb_face_out).add(R.id.parent, bank).commit();
-            }catch(Exception e)
-            {
-                e.printStackTrace();
-                finish();
-            }
-            webView.setWebChromeClient(new PayUWebChromeClient(bank) {});
-        } catch (ClassNotFoundException e) {
-            webView.getSettings().setSupportMultipleWindows(true);
-            webView.getSettings().setJavaScriptCanOpenWindowsAutomatically(true);
-            webView.addJavascriptInterface(new Object() {
-                @JavascriptInterface
-                public void onSuccess() {
-                    onSuccess("");
-                }
 
-                @JavascriptInterface
-                public void onSuccess(final String result) {
+            @android.webkit.JavascriptInterface
+            public void onFailure() {
+                onFailure("");
+            }
+
+            @android.webkit.JavascriptInterface
+            public void onFailure(final String result) {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent();
+                        intent.putExtra("result", result);
+                        setResult(RESULT_CANCELED, intent);
+                        finish();
+                    }
+                });
+            }
+        }, "PayU");
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+        });
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+                if (url.contains("http://purplesq.com/events/")) {
+                    final String result = url.substring((url.indexOf("=")) + 1);
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -120,34 +94,10 @@ public class ProcessPaymentActivity extends FragmentActivity {
                             setResult(RESULT_OK, intent);
                             finish();
                         }
-//                }
                     });
                 }
-
-                @JavascriptInterface
-                public void onFailure() {
-                    onFailure("");
-                }
-
-                @JavascriptInterface
-                public void onFailure(final String result) {
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            Intent intent = new Intent();
-                            intent.putExtra("result", result);
-                            setResult(RESULT_CANCELED, intent);
-                            finish();
-                        }
-                    });
-                }
-            }, "PayU");
-
-            webView.setWebChromeClient(new WebChromeClient() {
-
-            });
-            webView.setWebViewClient(new WebViewClient());
-        }
+            }
+        });
 
         webView.getSettings().setJavaScriptEnabled(true);
         webView.getSettings().setDomStorageEnabled(true);
@@ -156,9 +106,9 @@ public class ProcessPaymentActivity extends FragmentActivity {
 
 
     @Override
-    public void onBackPressed(){
+    public void onBackPressed() {
         boolean disableBack = false;
-        if(cancelTransaction){
+        if (cancelTransaction) {
             cancelTransaction = false;
             Intent intent = new Intent();
             intent.putExtra("result", "Transaction canceled due to back pressed!");
@@ -172,8 +122,9 @@ public class ProcessPaymentActivity extends FragmentActivity {
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-        if(!disableBack) {
-            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);;
+        if (!disableBack) {
+            AlertDialog.Builder alertDialog = new AlertDialog.Builder(this);
+            ;
             alertDialog.setCancelable(false);
             alertDialog.setMessage("Do you really want to cancel the transaction ?");
             alertDialog.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
@@ -187,14 +138,12 @@ public class ProcessPaymentActivity extends FragmentActivity {
             alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
                 @Override
                 public void onClick(DialogInterface dialog, int which) {
-                   dialog.dismiss();
+                    dialog.dismiss();
                 }
             });
             alertDialog.show();
         }
     }
-
-
 
 
     @Override
